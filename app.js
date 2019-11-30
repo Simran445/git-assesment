37ce21753dff768f88aaa55c5f71f370344d4812
Simran Kaur <simran.kaur@mail.vinove.com>
2019-11-30 13:37:11 +0530

    Changes of develop merged

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 /**
  * A Print function
  */
+function printLine()
+{
+    return `Speak of the devil!`;
+}
+
 function printJumboTronLine()
 {
     return `Speak of the devil, and Devil is here !`;
@@ -10,6 +15,11 @@ function printJumboTronLine()
  * A Heading  function
  * @returns {string}
  */
+function addHeading()
+{
+    return `Don't count your chickens before the eggs have hatched`;
+}
+
 function addJumboTron()
 {
     return `Don't put all your eggs in one basket. Don't count your chickens before the eggs have hatched`;
